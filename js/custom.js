73a626ebdc1a60b367f228bddd3b84e268293e19
SixStringsCoder <rshanlon@gmail.com>
2017-07-29 13:51:25 -0700

    work on hero image, button color, start jQuery for info bands below

---
+++ js/custom.js
@@ -0,0 +1,20 @@
+/*
+  Created by Steve Hanlon on 7/29/17 for Github portfolio sit.
+*/
+
+
+// Show and hide text for shopping cart
+$('.fa-shopping-cart').hover(function() {
+    $('.store').slideToggle(1000);
+});
+
+
+//
+// $( ".fa-shopping-cart" ).hover(function() {
+//   $( ".store" ).animate({
+//     opacity: 0.9,
+//     height: "toggle"
+//   }, 500, function() {
+//     // Animation complete.
+//   });
+// });
